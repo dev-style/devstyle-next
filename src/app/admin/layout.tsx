0d337f0a48c4ea2f7656e1f49905b950ebcc340d
Dev-scott <sadoscott25@gmail.com>
2023-10-29 22:17:36 +0100

    refactor(app): #6 work on login

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { ToastProvider } from "../(client)/lib/toastProvider";
 import { Providers } from "./Provider";
+import { SessionProvider } from "next-auth/react";
+import { useLoadUserQuery } from "./redux/features/api/apiSlice";
+import { FC, useEffect } from "react";
+
+import socketIO from "socket.io-client";
+const ENDPOINT = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
+const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 export default function RootLayout({
   children
@@ -9,10 +16,32 @@ export default function RootLayout({
 }) {
   return (
     <Providers>
-      <div>
+      <SessionProvider>
+        <Custom>
+          <div>
+            {children}
+          </div>
+        </Custom>
         <ToastProvider />
-        {children}
-      </div>
+      </SessionProvider>
     </Providers>
   );
 }
+
+const Custom: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isLoading } = useLoadUserQuery({});
+
+  useEffect(() => {
+    socketId.on("connection", () => {});
+  }, []);
+
+  return (
+    <div>
+      {isLoading
+        ? <div>Loading</div>
+        : <div>
+            {children}{" "}
+          </div>}
+    </div>
+  );
+};
